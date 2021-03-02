cd7d9593436f3af49b32e659672e9d26c2255685
Shutarou Takayama <hi.mountain110916@gmail.com>
2021-03-03 03:12:25 +0900

    カラーの変更

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Color.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Color.kt
@@ -17,7 +17,10 @@ package com.example.androiddevchallenge.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val purple200 = Color(0xFFBB86FC)
-val purple500 = Color(0xFF6200EE)
-val purple700 = Color(0xFF3700B3)
+//val purple200 = Color(0xFFBB86FC)
+//val purple500 = Color(0xFF6200EE)
+//val purple700 = Color(0xFF3700B3)
 val teal200 = Color(0xFF03DAC5)
+val orange = Color(0xffff532f)
+val lightOrange = Color(0xffffc046)
+val darkOrange = Color(0xffc56000)
